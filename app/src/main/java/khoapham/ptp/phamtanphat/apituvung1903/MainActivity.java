8d2b26095974cbdd5f0098b541ce4d623b125bd2
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-11 21:15:58 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/apituvung1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/apituvung1903/MainActivity.java
@@ -2,9 +2,17 @@ package khoapham.ptp.phamtanphat.apituvung1903;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import khoapham.ptp.phamtanphat.apituvung1903.Api.connection.APICallback;
 import khoapham.ptp.phamtanphat.apituvung1903.Api.connection.Dataresponse;
+import khoapham.ptp.phamtanphat.apituvung1903.Api.modelapi.TuvungAPus;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         APICallback apiCallback = Dataresponse.initRequestToServer();
+        Call<List<TuvungAPus>> tuvungcallback = apiCallback.getDataTuvung();
+        tuvungcallback.enqueue(new Callback<List<TuvungAPus>>() {
+            @Override
+            public void onResponse(Call<List<TuvungAPus>> call, Response<List<TuvungAPus>> response) {
+                ArrayList<TuvungAPus> mangtuvung = (ArrayList<TuvungAPus>) response.body();
+                Log.d("BBB",mangtuvung.size() + "");
+            }
 
+            @Override
+            public void onFailure(Call<List<TuvungAPus>> call, Throwable t) {
+
+            }
+        });
     }
 }
