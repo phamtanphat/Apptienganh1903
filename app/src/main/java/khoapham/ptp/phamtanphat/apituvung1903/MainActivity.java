742d8711e68724a48c454ccc6d878cffc355c411
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-13 21:15:27 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/apituvung1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/apituvung1903/MainActivity.java
@@ -3,7 +3,10 @@ package khoapham.ptp.phamtanphat.apituvung1903;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,19 +24,44 @@ public class MainActivity extends AppCompatActivity {
 
     ListView lvTuvung;
     TuvungAdapter tuvungAdapter;
+    RelativeLayout relativeFormtrue,relativeFormfalse;
+    Button btnForm,btnAddWord,btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
+        event();
         getWord();
 
+    }
 
+    private void event() {
+        btnForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeFormtrue.setVisibility(View.VISIBLE);
+                relativeFormfalse.setVisibility(View.GONE);
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeFormtrue.setVisibility(View.GONE);
+                relativeFormfalse.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     private void initView() {
         lvTuvung = findViewById(R.id.listviewWord);
+        relativeFormfalse = findViewById(R.id.relativeFormfalse);
+        relativeFormtrue = findViewById(R.id.relativeFormtrue);
+        btnForm = findViewById(R.id.buttonForm);
+        btnAddWord = findViewById(R.id.buttonAddword);
+        btnCancel = findViewById(R.id.buttonCancel);
     }
 
     private void getWord() {
